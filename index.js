5dd0c1575c5ac48fd0e7f557f8b67e0fc4cb8f92
moranja <jeffadammoran@gmail.com>
2019-04-08 16:59:05 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string) {
+    return string.replace(/[^a-z0-9-'\s]+/gi, '');
+  }
+
+  static titleize (string) {
+    const string_array = string.split(' ')
+    const output_array = []
+    const doNotTitleize = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    string_array.forEach(function(word, index){
+      if (index === 0 || doNotTitleize.includes(word) !== true) {
+        output_array.push(Formatter.capitalize(word))
+      } else {
+        output_array.push(word)
+      }
+    })
+    return output_array.join(' ')
+  }
 }
